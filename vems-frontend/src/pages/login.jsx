075aa804137fea799543532528d951136fa7b68e
Rancy9 <huisze4793@gmail.com>
2025-12-29 21:50:37 +0800

    homepage

--- vems-frontend/src/pages/login.jsx
+++ vems-frontend/src/pages/login.jsx
@@ -9,7 +9,7 @@ function LoginPage(){
 
     const handleLogin = (e)=> {
         e.preventDefault();
-        navigate('/dashboard')
+        navigate('/homepage')
         alert(`Login Ateempt \nUser ID: ${userId}\nStatus: Authenticating...`);
     };
 
